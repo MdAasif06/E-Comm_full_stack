7f66c8dcf93197928016346d81d039b8430dc30d
MdAasif06 <abdurm477@gmail.com>
2026-03-03 10:12:02 +0530

    remove some bugs

--- stridex-frontend/src/api/axios.js
+++ stridex-frontend/src/api/axios.js
@@ -2,14 +2,14 @@ import axios from "axios";
 
 const API = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
-  // headers: {
-  //   "Content-Type": "application/json",
-  // },
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 // Token auto attach
 API.interceptors.request.use((req) => {
-  const user = JSON.parse(localStorage.getItem("user"));
+  const user = JSON.parse(localStorage.getItem("userInfo")); // 🔑 use correct key
 
   if (user?.token) {
     req.headers.Authorization = `Bearer ${user.token}`;
